52020e337854958f092686a92d9770c318dba25d
be <nebxyang@163.com>
2017-01-23 15:00:33 +0800

    Initial commit

---
+++ app/src/main/java/com/soaringnova/novascenic/ARNavigation.java
@@ -0,0 +1,13 @@
+package com.soaringnova.novascenic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ARNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arnavigation);
+    }
+}
